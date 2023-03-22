95ce95c75516ec34d0ab4a0a4d91e3cd487f12d5
Arnold Ondiba <arnoldondiba@gmail.com>
2023-03-22 21:13:28 +0300

    ¨0-putchar.c¨

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* print_alphabet - prints alphabets
+*
+*Return: Always 0.
+*/
+
+
+		 void print_alphabet(void)
+	{
+		char cara = 'a';
+
+		for ( ; cara <= 'z'; cara++)
+		{
+			_putchar(cara);
+		}
+
+		_putchar('\n');
+	}
