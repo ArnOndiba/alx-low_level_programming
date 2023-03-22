483dbde5fb00c5ca6a05940bf8f592ddde3e810e
Arnold Ondiba <arnoldondiba@gmail.com>
2023-03-22 21:11:51 +0300

    ¨0-putchar.c¨

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * main - prints _putchar
+ *
+ *Return: Always 0.
+ */
+		int main(void)
+	{
+		char str[] = "_putchar";
+
+		int i = 0;
+
+		for ( ; i < 8; i++)
+		{
+			_putchar(str[i]);
+		}
+
+		_putchar('\n');
+
+		return (0);
+	}
