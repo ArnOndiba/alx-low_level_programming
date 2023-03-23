77941dd11f27f85451e3910b131e45de7938ae09
Arnold Ondiba <arnoldondiba@gmail.com>
2023-03-23 20:12:47 +0300

    ¨3-print_numbers.c¨

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers trough 0 to 9
+ *
+ * Return: 0
+ */
+
+void print_numbers(void)
+{
+	char n = 0;
+
+	for ( ; n < 10 ; n++)
+	{
+		_putchar(n + '0');
+	}
+	_putchar('\n');
+}
