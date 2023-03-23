00da7faaf987a4d175e37af63f9cabed85182b15
Arnold Ondiba <arnoldondiba@gmail.com>
2023-03-23 22:01:33 +0300

    ¨8-main.c¨

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_square - make a square
+ * @size: length of square
+ * Return: void
+*/
+	void print_square(int size)
+	{
+		int i = 0, j;
+
+		while (i < size && size > 0)
+		{
+			j = 0;
+			while (j < size)
+			{
+				_putchar('#');
+				j++;
+			}
+				_putchar('\n');
+				i++;
+		}
+			if (i == 0)
+			_putchar('\n');
+	}
