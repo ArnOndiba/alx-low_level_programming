4c98715fa46a52cb25ea7afb9a0ed43c76f8ad02
Arnold Ondiba <arnoldondiba@gmail.com>
2023-04-17 08:27:32 +0300

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,13 +10,14 @@ void puts2(char *str)
 {
 	int i = 0;
 
-	while (str[i] != '\0')
+	for(;str[i] != '\0';i++)
 	{
-		if (i % 2 == 0)
+		if(i % 2 == 0)
 		{
-			_putchar(i);
+			_putchar(str[i]);
+			i++;
 		}
-		i++;
 	}
 	_putchar('\n');
 }
+
