134fc875487073b8eb6fac31d640591445887a82
Arnold Ondiba <arnoldondiba@gmail.com>
2023-06-08 08:24:45 +0300

    2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * _strlen_recursion -prints string length
+ * @s: input variable
+ *
+ * Return: string length
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+
+	return (1 + _strlen_recursion(s + 1));
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -9,3 +9,5 @@
 	int _putchar(char c);
 
 	void _print_rev_recursion(char *s);
+
+	int _strlen_recursion(char *s);
